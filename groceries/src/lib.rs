d393187f7eb555c39c687c6d82782a6d2f17c169
ahabchi <amine.habchi.01@gmail.com>
2025-05-08 12:51:54 +0100

    groceries

---
+++ groceries/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    &slice[index]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2+2, 4);
+    }
+}
