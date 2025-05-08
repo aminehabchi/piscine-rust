a4c1ca807dae7da0da7c628490f2cae3e8176623
ahabchi <amine.habchi.01@gmail.com>
2025-05-08 11:54:01 +0100

    looping

--- looping/target/debug/deps/looping-b52237923c286e5a.d
+++
@@ -1,5 +0,0 @@
-/home/ahabchi/Desktop/piscine-rust/looping/target/debug/deps/looping-b52237923c286e5a: src/main.rs
-
-/home/ahabchi/Desktop/piscine-rust/looping/target/debug/deps/looping-b52237923c286e5a.d: src/main.rs
-
-src/main.rs:
--- looping/target/debug/looping.d
+++
@@ -1,1 +0,0 @@
-/home/ahabchi/Desktop/piscine-rust/looping/target/debug/looping: /home/ahabchi/Desktop/piscine-rust/looping/src/main.rs
