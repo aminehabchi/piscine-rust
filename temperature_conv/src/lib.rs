0ace8e259652f629c2bb9f8f693878075c6969bf
ahabchi <amine.habchi.01@gmail.com>
2025-05-08 11:08:28 +0100

    temp

---
+++ temperature_conv/src/lib.rs
@@ -0,0 +1,28 @@
+/*
+Formula	
+(33.8°F − 32) × 5/9 = 1°C
+*/
+
+pub fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - (32 as f64))*((5/9) as f64)
+}
+
+/*
+Formula	
+(c × 9/5) + 32 = f
+*/
+
+pub fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c*((9/5) as f64)) + (32 as f64)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+       
+        assert_eq!(2+2, 4);
+    }
+}
