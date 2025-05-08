de144807895f4fca5da1f5f887a415f94791a4aa
ahabchi <amine.habchi.01@gmail.com>
2025-05-08 11:21:32 +0100

    temp3

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -3,8 +3,11 @@ Formula
 (33.8°F − 32) × 5/9 = 1°C
 */
 
+fn to_fixed2(n: f64) -> f64{
+    (n * 100.0).round() / 100.0
+}
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0)*(5.0/9.0)
+    to_fixed2((f - 32.0)*(5.0/9.0))
 }
 
 /*
@@ -13,7 +16,7 @@ Formula
 */
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c*((9/5) as f64)) + (32 as f64)
+    to_fixed2((c*((9/5) as f64)) + (32 as f64))
 }
 
 #[cfg(test)]
