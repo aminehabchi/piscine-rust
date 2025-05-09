31b4446094350ce2592ede702481765dbe5d4a92
ahabchi <amine.habchi.01@gmail.com>
2025-05-09 10:45:05 +0100

    to_url

---
+++ to_url/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn to_url(s: &str) -> String {
+    let mut new_s:String=String::from("");
+    for ch in s.chars(){
+        if ch==' '{
+            new_s.push_str("20%");
+        }else{
+            new_s.push(ch);
+        }
+    }
+    new_s
+}
