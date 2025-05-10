d64436f7b0b0c20ce3498c998cdc5fe72a25c126
ahabchi <amine.habchi.01@gmail.com>
2025-05-10 17:08:39 +0100

    simple_hash

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut map:HashMap<&'a str, usize> = HashMap::new();
+    for w in words{
+        *map.entry(w).or_insert(0) += 1;
+    }
+    map
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    map.len()
+}
