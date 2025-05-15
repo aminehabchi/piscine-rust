339416852d2b9901f1f54043a8e0d8e49143c4c7
aminehabchi <amine.habchi.01@gmail.com>
2025-05-15 11:51:39 +0100

    fix

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -6,29 +6,31 @@ pub enum Security {
     UnexpectedUrl,
 }
 
-
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match security_level {
+        Security::Unknown => match server {
+            Ok(url) => url.to_string(),
+            Err(_) => panic!(), 
+        },
 
-    match security_level{
-        Security::Unknown       => match server{
-                                        Ok(m) => return m.to_string(),
-                                        Err(_) => panic!(),
-                                    },
-        Security::Message       => match server{
-                                        Ok(m) => return m.to_string(),
-                                        Err(_e)=> panic!("ERROR: program stops"),
-                                    },
-        Security::Warning       => match server{
-                                        Ok(m) => return m.to_string(),
-                                        Err(_)=> return "WARNING: check the server".to_string(),
-                                    },
-        Security::NotFound      =>  match server{
-                                        Ok(m) => return m.to_string(),
-                                        Err(e)=> return format!("Not found: {}", e),
-                                    },
-        Security::UnexpectedUrl => match server{
-                                        Ok(url) => panic!("{}", url),
-                                        Err(err) => err.to_string(),
-                                    },
+        Security::Message => match server {
+            Ok(url) => url.to_string(),
+            Err(_) => panic!("ERROR: program stops"),
+        },
+
+        Security::Warning => match server {
+            Ok(url) => url.to_string(),
+            Err(_) => "WARNING: check the server".to_string(),
+        },
+
+        Security::NotFound => match server {
+            Ok(url) => url.to_string(),
+            Err(err) => format!("Not found: {}", err),
+        },
+
+        Security::UnexpectedUrl => match server {
+            Ok(url) => panic!("{}", url),
+            Err(err) => err.to_string(),
+        },
     }
 }
