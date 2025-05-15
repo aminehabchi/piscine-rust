93203dd1fb54e466994a773d66edb0ea60566392
aminehabchi <amine.habchi.01@gmail.com>
2025-05-15 11:05:21 +0100

    unwrap

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -11,15 +11,15 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
     match security_level{
         Security::Unknown       => match server{
                                         Ok(m) => return m.to_string(),
-                                        Err(e)=> return e.to_string(),
+                                        Err(e)=> panic!(""),
                                     },
         Security::Message       => match server{
                                         Ok(m) => return m.to_string(),
-                                        Err(_e)=> return "ERROR: program stops".to_string(),
+                                        Err(_e)=> panic!("ERROR: program stops"),
                                     },
         Security::Warning       => match server{
                                         Ok(m) => return m.to_string(),
-                                        Err(_e)=> return "WARNING: check the server".to_string(),
+                                        Err(e)=> return "WARNING: check the server".to_string(),
                                     },
         Security::NotFound      =>  match server{
                                         Ok(m) => return m.to_string(),
@@ -27,7 +27,7 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
                                     },
         Security::UnexpectedUrl => match server{
                                         Ok(m) => return m.to_string(),
-                                        Err(e)=> return e.to_string(),
+                                        Err(e)=> panic!("{}",e.to_string())
                                     },
     }
 }
