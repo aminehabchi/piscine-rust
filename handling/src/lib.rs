bd458bed51a55c28d72406afbd5445369d042c32
aminehabchi <amine.habchi.01@gmail.com>
2025-05-16 09:42:52 +0100

    done

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,10 +1,9 @@
 use std::path::Path;
 use std::fs::OpenOptions;
-use std::io::{Write};
-
+use std::io::Write;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    let file =OpenOptions::new().write(true).create(true).open(path);
+    let mut file =OpenOptions::new().write(true).create(true).append(true).open(path).unwrap();
 
-   let _ = file.unwrap().write_all(content.as_bytes());
+     write!(file,"{}",content).unwrap();
 }
