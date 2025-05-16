e042253b65e04b44d3ffb7819bf843574c30fef2
aminehabchi <amine.habchi.01@gmail.com>
2025-05-16 11:50:52 +0100

    done

--- cipher/src/lib.rs
+++ cipher/src/lib.rs
@@ -4,27 +4,28 @@ pub struct CipherError {
 }
 
 pub fn cipher(original: &str, ciphered: &str) -> Result<(), CipherError> {
-    let c:CipherError=CipherError{
-        expected:original.to_string(),
-    };
-    if original.len()!=ciphered.len(){
-        return Err(c);
-    }
-
+   
     let mut s:String=String::new();
-    let new=original.to_lowercase();
-    for ch in new.bytes(){
+   
+    for ch in original.bytes(){
         if ch.is_ascii_alphabetic() {
-            let pos = ch as i32 - 'a' as i32;
+            let mut base='a';
+            if ch!=ch.to_ascii_lowercase(){
+                base='A';
+            }
+            let pos = ch as i32 - base as i32;
             let new_pos = 25 - pos;  
-            let new_char = (new_pos + 'a' as i32) as u8 as char;
+            let new_char = (new_pos + base as i32) as u8 as char;
               s.push(new_char);
         }else{
             s.push(ch as char);
         }
     }  
-    
-    if s != ciphered.to_lowercase() {
+    let c:CipherError=CipherError{
+        expected:s.to_string(),
+    };
+
+    if s != ciphered {
         return Err(c);
     }
 
