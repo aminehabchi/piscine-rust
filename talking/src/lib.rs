c1efe92040a44c6089f6926abd1d03e9e1068224
aminehabchi <amine.habchi.01@gmail.com>
2025-05-17 11:25:39 +0100

    done

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,9 +1,24 @@
+fn is_cap(t :&str)->bool{
+    let s=t.to_uppercase();
+    return t==s;
+}
+
 pub fn talking(text: &str) -> &str {
-    match text {
-        "LEAVE ME ALONE!" =>"There is no need to yell, calm down!",
-        "Is everything ok with you?"=>"Sure.",
-       "HOW ARE YOU?"=>"Quiet, I am thinking!",
-        ""=>"Just say something!",
-         &_ => "Interesting",
+    if text==""{
+     return "Just say something!";
     }
+    if is_cap(text) && text.contains('?'){
+        return "Quiet, I am thinking!";
+    }
+
+    if !is_cap(text) && text.contains('?'){
+        return "Sure.";
+    }
+
+   if is_cap(text){
+    return "There is no need to yell, calm down!";
+   }
+
+
+   "Interesting"
 }
