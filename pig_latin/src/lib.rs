a4b5daa7b76195a8452ef82a292d13518c93b5d4
aminehabchi <amine.habchi.01@gmail.com>
2025-05-17 12:47:46 +0100

    doen

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn pig_latin(text: &str) -> String {
+    let vowels = "aeiou";
+    let mut j: usize = 0;
+    if text.find("qu") == Some(1) {
+        if let Some(first_char) = text.chars().next() {
+            if !vowels.contains(first_char) {
+                let (l, r) = text.split_at(3);
+                let mut s = String::new();
+                s.push_str(r);
+                s.push_str(l);
+                s.push_str("ay");
+                return s;
+            }
+        }
     }
+
+    for (i, ch) in text.chars().enumerate() {
+        if vowels.contains(ch) {
+            j = i;
+            break;
+        }
+    }
+
+    let (l, r) = text.split_at(j);
+    let mut s = String::new();
+    s.push_str(r);
+    s.push_str(l);
+    s.push_str("ay");
+    s
 }
