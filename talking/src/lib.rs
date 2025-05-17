98f927e704886ef9b6e49369d2af63dd28cca591
aminehabchi <amine.habchi.01@gmail.com>
2025-05-17 11:39:36 +0100

    done

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -3,23 +3,25 @@ fn is_cap(t: &str) -> bool {
     has_letters && t.chars().all(|c| !c.is_alphabetic() || c.is_uppercase())
 }
 
+
 pub fn talking(text: &str) -> &str {
-    if text.trim().is_empty(){
-     return "Just say something!";
+    let trimmed = text.trim();
+
+    if trimmed.is_empty() {
+        return "Just say something!";
     }
 
-    if is_cap(text) && text.contains('?'){
+    if is_cap(trimmed) && trimmed.ends_with('?') {
         return "Quiet, I am thinking!";
     }
 
-    if text.contains('?'){
+    if trimmed.ends_with('?') {
         return "Sure.";
     }
 
-   if is_cap(text){
-    return "There is no need to yell, calm down!";
-   }
+    if is_cap(trimmed) {
+        return "There is no need to yell, calm down!";
+    }
 
-
-   "Interesting"
+    "Interesting"
 }
