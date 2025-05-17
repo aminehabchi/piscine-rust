2179053fef02b398c950454d94f5a22c35af565b
aminehabchi <amine.habchi.01@gmail.com>
2025-05-17 10:35:50 +0100

    done

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -1,19 +1,19 @@
 
 
 pub fn score(s :&str)->u64{
-    let mut x:usize=0;
+    let mut x:u64=0;
     
     let arr :Vec<&str>=["AEIOULNRST","DG","BCMP","FHVWY","K","JX","QZ"].to_vec();
-
+    let score:Vec<u64>=[1,2,3,4,5,8,10].to_vec();
     for ch in s.chars(){
         for i in 0..arr.len(){
             if arr[i].contains(ch.to_uppercase().next().unwrap()){
-                x+=i+1;
+                x+=score[i];
                 break;
             }
         }
     }
 
 
-    x as u64
+    x 
 }
