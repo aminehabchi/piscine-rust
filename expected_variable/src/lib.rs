8b9fbf0413646bd3f4ce5052050f5e528493b29b
aminehabchi <amine.habchi.01@gmail.com>
2025-05-17 17:45:22 +0100

    done

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -27,21 +27,17 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
 }
 
 
-pub fn expected_variable(a: &str, b: &str) -> Option<String> {
-    if b.is_empty() {
+pub fn expected_variable(a:&str,b:&str)->Option<String>{
+    let a1:String=a.to_string().to_lowercase();
+    let b1:String=b.to_string().to_lowercase();
+    let n:i32=b.len() as i32- edit_distance(&a1,&b1) as i32 ;
+    let s:f64=n as f64 * 100.0/b.len() as f64;
+
+    let x:i32=s.round() as i32;
+
+    if x<50{
         return None;
     }
+    Some(x.to_string()+"%")
 
-    let a1 = a.to_lowercase();
-    let b1 = b.to_lowercase();
-
-    let distance = edit_distance(&a1, &b1);
-    let similarity = ((b.len() - distance) as f64 * 100.0) / b.len() as f64;
-    let percentage = similarity.round() as i32;
-
-    if percentage <= 50 {
-        None
-    } else {
-        Some(format!("{}%", percentage))
-    }
 }
