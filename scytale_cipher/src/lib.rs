c688972bc335d3da81386029219c79fd1ebb686c
aminehabchi <amine.habchi.01@gmail.com>
2025-05-18 10:12:04 +0100

    done

---
+++ scytale_cipher/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn scytale_cipher(message: String, i: u32) -> String {
+    let mut s:String=String::new();
+    let m:Vec<u8>=message.bytes().collect();
+    let l:usize=m.len();
+
+    for index in 0..i as usize{
+        s.push(m[index] as char);
+        if index+(i as usize)<l{
+             s.push(m[index+i as usize] as char);
+        }else{
+            s.push(' ');
+        }
+    } 
+
+    let s= s.trim_end_matches(' ').to_string();
+
+    s
+}
