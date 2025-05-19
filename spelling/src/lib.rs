5db14f2749416aac5a441d81f1d0c77fec61b25f
aminehabchi <amine.habchi.01@gmail.com>
2025-05-19 10:49:41 +0100

    done

---
+++ spelling/src/lib.rs
@@ -0,0 +1,77 @@
+
+fn less_then_100(a:u64,b:u64)->String{
+     let numbers: Vec<String> = vec![
+        "".to_string(),
+        "one".to_string(),
+        "two".to_string(),
+        "three".to_string(),
+        "four".to_string(),
+        "five".to_string(),
+        "six".to_string(),
+        "seven".to_string(),
+        "eight".to_string(),
+        "nine".to_string(),
+        "ten".to_string(),
+        "eleven".to_string(),
+        "twelve".to_string(),
+        "thirteen".to_string(),
+        "fourteen".to_string(),
+        "fifteen".to_string(),
+        "sixteen".to_string(),
+        "seventeen".to_string(),
+        "eighteen".to_string(),
+        "nineteen".to_string(),
+        "twenty".to_string(),
+    ];
+
+    let tens = vec![
+    "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    ];
+
+    if a<=2{
+        return numbers[(a*10) as usize + b as usize].to_string();
+    }
+
+    let mut result:String=tens[a as usize].to_string();
+    result.push('-');
+    result.push_str(&numbers[b as usize].to_string());
+
+    result
+}
+
+pub fn spell(m: u64) -> String {
+    let scales = vec![
+        "", "thousand", "million", "billion"
+    ];
+
+    let mut arr:Vec<String>=Vec::new();
+    let mut n:u64=m;
+    let mut counter:usize=0;
+    while n>0{
+        let a=n%10;
+        n/=10;
+        let b=n%10;
+        n/=10;
+        let c=n%10;
+        n/=10;
+        
+        if a!=0 || b!=0 || c!=0{
+            arr.push(scales[counter].to_string());
+        }
+
+        arr.push(less_then_100(b,a).to_string());
+
+        if c!=0{
+            arr.push("hundred".to_string());
+            arr.push(less_then_100(0,c).to_string());
+        }
+
+
+        counter+=1;
+    }
+    
+    arr.into_iter()
+        .rev()
+        .collect::<Vec<_>>()
+        .join(" ")
+}
---
+++ spelling/src/main.rs
@@ -0,0 +1,13 @@
+use spelling::*;
+
+fn main() {
+    println!("{}", spell(0));
+    println!("{}", spell(1));
+    println!("{}", spell(14));
+     println!("{}", spell(96));
+    println!("{}", spell(100));
+     println!("{}", spell(101));
+    println!("{}", spell(348));
+     println!("{}", spell(1002));
+    println!("{}", spell(10000000));
+}
