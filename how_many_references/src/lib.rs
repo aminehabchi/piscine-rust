ab5976277efbed3d0bf1822b521c8e1d7b94d4d5
ahabchi <amine.habchi.01@gmail.com>
2025-06-26 16:17:58 +0100

    done

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,29 @@
+pub use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Node {
+        Node { ref_list: ref_list }
+    }
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        let mut i: usize = 0;
+
+        while i < self.ref_list.len() {
+            if *self.ref_list[i] == *element {
+                self.ref_list.remove(i);
+            } else {
+                i += 1;
+            }
+        }
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+    Rc::strong_count(ref_list)
+}
