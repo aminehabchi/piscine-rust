be6555a1f158ccc6567a265c37d130b53ee9b33f
ahabchi <amine.habchi.01@gmail.com>
2025-06-28 11:27:42 +0100

    done

--- ref_cell/src/lib.rs
+++ ref_cell/src/lib.rs
@@ -24,14 +24,14 @@ impl Worker {
 impl Logger for Worker {
     fn warning(&self, msg: &str) {
         self.mapped_messages.borrow_mut().insert("Warning".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Warning: {}", msg));
+        self.all_messages.borrow_mut().push(format!("{}", msg));
     }
     fn info(&self, msg: &str) {
         self.mapped_messages.borrow_mut().insert("Info".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Info: {}", msg));
+        self.all_messages.borrow_mut().push(format!("{}", msg));
     }
     fn error(&self, msg: &str) {
         self.mapped_messages.borrow_mut().insert("Error".to_string(), msg.to_string());
-        self.all_messages.borrow_mut().push(format!("Error: {}", msg));
+        self.all_messages.borrow_mut().push(format!("{}", msg));
     }
 }
