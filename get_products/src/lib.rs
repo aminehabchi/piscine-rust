b8e5d0df4470924a23935974b53ce5eae229fb39
ahabchi <amine.habchi.01@gmail.com>
2025-07-05 16:52:08 +0100

    done

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -1,4 +1,7 @@
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    if arr.len() == 1 {
+        return arr;
+    }
     let all: usize = arr.iter().product();
     let mut ar = arr.clone();
     for i in 0..ar.len() {
