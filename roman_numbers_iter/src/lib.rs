6408ffb8955a922e04ab42dd8312074f60445cef
ahabchi <amine.habchi.01@gmail.com>
2025-07-06 08:52:56 +0100

    done

--- roman_numbers_iter/src/lib.rs
+++ roman_numbers_iter/src/lib.rs
@@ -1,6 +1,3 @@
-use crate::RomanDigit::*;
-use std::iter::Iterator;
-
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum RomanDigit {
     Nulla,
@@ -15,21 +12,23 @@ pub enum RomanDigit {
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct RomanNumber {
-    value: u32,
+    pub value: u32,
     pub digits: Vec<RomanDigit>,
 }
+
+use crate::RomanDigit::*;
 
 impl From<u32> for RomanDigit {
     fn from(n: u32) -> Self {
         match n {
-            1 => RomanDigit::I,
-            5 => RomanDigit::V,
-            10 => RomanDigit::X,
-            50 => RomanDigit::L,
-            100 => RomanDigit::C,
-            500 => RomanDigit::D,
-            1000 => RomanDigit::M,
-            _ => RomanDigit::Nulla,
+            1 => I,
+            5 => V,
+            10 => X,
+            50 => L,
+            100 => C,
+            500 => D,
+            1000 => M,
+            _ => Nulla,
         }
     }
 }
@@ -39,24 +38,24 @@ impl From<u32> for RomanNumber {
         if n == 0 || n >= 50000 {
             return RomanNumber {
                 value: 0,
-                digits: vec![RomanDigit::Nulla],
+                digits: vec![Nulla],
             };
         }
 
         let roman: Vec<(u32, [RomanDigit; 2])> = vec![
             (1000, [M, Nulla]),
-            (900,  [C, M]),
-            (500,  [D, Nulla]),
-            (400,  [C, D]),
-            (100,  [C, Nulla]),
-            (90,   [X, C]),
-            (50,   [L, Nulla]),
-            (40,   [X, L]),
-            (10,   [X, Nulla]),
-            (9,    [I, X]),
-            (5,    [V, Nulla]),
-            (4,    [I, V]),
-            (1,    [I, Nulla]),
+            (900, [C, M]),
+            (500, [D, Nulla]),
+            (400, [C, D]),
+            (100, [C, Nulla]),
+            (90, [X, C]),
+            (50, [L, Nulla]),
+            (40, [X, L]),
+            (10, [X, Nulla]),
+            (9, [I, X]),
+            (5, [V, Nulla]),
+            (4, [I, V]),
+            (1, [I, Nulla]),
         ];
 
         let mut result = Vec::new();
@@ -87,6 +86,8 @@ impl Iterator for RomanNumber {
         if self.value >= 50000 {
             return None;
         }
-        Some(RomanNumber::from(self.value))
+        let next = RomanNumber::from(self.value);
+        Some(next)
     }
 }
+
