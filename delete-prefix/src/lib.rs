cfe907a0bf3b358b0f51c8621c4688be1ade5e94
ahabchi <amine.habchi.01@gmail.com>
2025-07-06 07:35:00 +0100

    done

---
+++ delete-prefix/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn delete_prefix<'a>(prefix: &'a str, s: &'a str) -> Option<&'a str> {
+    if prefix.len() > s.len() {
+        return None;
+    }
+    for _i in 0..prefix.len() {
+        if s.chars().next() != prefix.chars().next() {
+            return None;
+        }
+    }
+    Some(&s[prefix.len()..])
+}
